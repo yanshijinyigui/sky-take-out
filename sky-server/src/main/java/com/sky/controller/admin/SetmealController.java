377f90c60cad07eefa913c81aaef28fba43210b7
yanqiu <cyz18956817280@gmail.com>
2023-12-07 10:36:07 +0800

    默认

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
---
+++ sky-server/src/test/java/com/sky/skyApplicationTest.java
@@ -0,0 +1,109 @@
+package com.sky;
+
+import com.sky.result.Result;
+import com.sky.utils.HttpClientUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.codehaus.jettison.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class skyApplicationTest {
+
+
+@Autowired
+    RedisTemplate redisTemplate;
+
+
+    @Test
+    public  void testRedisTemplate(){
+        System.out.println("afacsbsbw"+redisTemplate);
+        //string数据操作
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        //hash类型的数据操作
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        //list类型的数据操作
+        ListOperations listOperations = redisTemplate.opsForList();
+        //set类型数据操作
+        SetOperations setOperations = redisTemplate.opsForSet();
+        //zset类型数据操作
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+
+    }
+
+    /**
+     * 操作字符串类型的数据
+     */
+    @Test
+    public void testString(){
+        // set get setex setnx
+        redisTemplate.opsForValue().set("name","小明");
+        String city = (String) redisTemplate.opsForValue().get("name");
+        System.out.println(city);
+        redisTemplate.opsForValue().set("code","1234",3, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().setIfAbsent("lock","1");
+        redisTemplate.opsForValue().setIfAbsent("lock","2");
+    }
+
+
+    /**
+     * 测试通过httpclient发送POST方式的请求
+     */
+    @Test
+    public void testPOST() throws Exception{
+        // 创建httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        //创建请求对象
+        HttpPost httpPost = new HttpPost("http://localhost:8080/admin/employee/login");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username","admin");
+        jsonObject.put("password","123456");
+
+        StringEntity entity = new StringEntity(jsonObject.toString());
+        //指定请求编码方式
+        entity.setContentEncoding("utf-8");
+        //数据格式
+        entity.setContentType("application/json");
+        httpPost.setEntity(entity);
+
+        //发送请求
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+
+        //解析返回结果
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("响应码为：" + statusCode);
+
+        HttpEntity entity1 = response.getEntity();
+        String body = EntityUtils.toString(entity1);
+        System.out.println("响应数据为：" + body);
+
+        //关闭资源
+        response.close();
+        httpClient.close();
+    }
+@Test
+    public void pp()throws IOException {
+    Map<String,String>pp=new HashMap<>();
+    pp.put("username","admin");
+    pp.put("password","123456");
+    String s = HttpClientUtil.doPost4Json("http://localhost:8080/admin/employee/login", pp);
+    System.out.println(s);
+    
+}
+
+}
