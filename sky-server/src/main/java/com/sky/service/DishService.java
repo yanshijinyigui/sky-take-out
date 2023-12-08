24d9c1c43f5b8fbe5227d97d55dc8487f030c353
yanqiu <cyz18956817280@gmail.com>
2023-12-08 09:48:07 +0800

    默认

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -53,4 +53,13 @@ public interface DishService {
      * @return
      */
     List<Dish> SelectDishByCategoryId(String categoryId);
+
+
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
